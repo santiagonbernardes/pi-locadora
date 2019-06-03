58aecb94aa512a0fceec997d85c78d8dd7d20514
Santiago Bernardes <santiago.bernardes@zgsolucoes.com.br>
2019-06-03 13:33:08 -0300

    Removendo arquivos relacionados ao lado do cliente e transformando este projeto no projeto servidor. (Correção)

--- src/main/java/br/com/senaigo/locadora/interfaces/PersisteDados.java
+++
@@ -1,9 +0,0 @@
-package br.com.senaigo.locadora.interfaces;
-
-public interface PersisteDados {
-
-    //Métodos
-    void monteObjeto(String dadosDoObjeto);
-
-    String desmonteObjeto();
-}
--- src/main/java/br/com/senaigo/locadora/main/Main.java
+++ src/main/java/br/com/senaigo/locadora/main/Main.java
@@ -2,16 +2,24 @@ package br.com.senaigo.locadora.main;
 
 import br.com.senaigo.locadora.comunicacao.ServidorTcp;
 import br.com.senaigo.locadora.controller.ServerTcpController;
-import br.com.senaigo.locadora.view.TelaSelecaoAplicacao;
 
 import java.io.IOException;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-            
-            TelaSelecaoAplicacao tela = new TelaSelecaoAplicacao();
-            tela.setVisible(true);
-
+		try {
+			ServidorTcp servidor = new ServidorTcp(7777);
+			System.out.println("Servidor online.");
+			while (true) {
+				String requisicao = servidor.receberMensagem();
+				System.out.println("Solicitação do cliente:" + requisicao);
+				ServerTcpController controller = new ServerTcpController(requisicao);
+				String resposta = controller.atendaRequisicao();
+				servidor.enviarMensagem(resposta);
+			}
+		} catch (Exception e) {
+			System.out.println("Erro no servidor: " + e.getMessage());
+		}
 	}
 }
